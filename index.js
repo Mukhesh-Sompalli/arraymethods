b7682fbf77f600d269bfe28acdcfb499baf3bd0a
Mukhesh-Sompalli <mukheshsompalli02@gmail.com>
2024-08-23 21:55:50 +0530

    add

---
+++ index.js
@@ -0,0 +1,93 @@
+// let x=[1,1,2,2,3,4,5]
+// let y=[6,4,3,7,8,9]
+// let z=[0,8,7,6,5,4]
+// let res=x.concat(y,z)
+// console.log(res)
+
+
+
+
+// const arr=[[1,2,3,4],[1,2,3,4]]
+// const res=arr.flat()
+// console.log(res)
+
+
+
+
+// let x=[10,20,30,40,50]
+// let y=[60,70,80,90]
+// y.pop(3);
+// console.log(y)
+
+
+
+// -----------------task-----------
+// join() 
+
+// let x=["Hello","world","this","is","javascript"]
+// let res=x.join()
+// console.log(res)
+
+// ---------------------------------------
+// delete
+
+// let num=[1,2,3,4,5]
+// let res=delete(num)
+// console.log(res)
+// ------------------------------------------
+// concat
+
+// let arr1=[1,2,3]
+// let arr2=[4,5,6]
+// let res=arr1.concat(arr2)
+// console.log(res)
+
+
+// -------------------------
+// flat
+
+// let array=[[1,2],[3,4],[5,[6,7]]]
+// let res=array.flat().flat()
+// console.log(res)
+
+
+// -----------------------------------
+// splice
+
+// let num=["apple","banana","orange","grape"]
+// let res=num.splice(0,2,"kiwi","mango")
+// console.log(res)
+// --------------------------
+// slice
+
+
+// let num=[1,2,3,4,5,6,7,8]
+// let res=num.slice(2,5)
+// console.log(res)
+
+
+
+
+
+
+// let array=[1,2,3,4,5]
+// let z=array.map(z=> z*2 )
+// console.log(z)
+
+// let x=[1,2,3,4,5]
+// c=0
+// let x=["hello","world","hellow"]
+// let z=x.filter(z=>  c++,x>=5)
+// console.log(z)
+
+
+// let x=[1,2,3,4,5]
+// let z=x.forEach(x=> {return console.log(x)}   
+// )
+
+
+let array
+
+
+
+
